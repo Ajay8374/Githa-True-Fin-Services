27857167b6b486a493f86496bc1231533f141784
Ajay8374 <132041577+Ajay8374@users.noreply.github.com>
2026-02-19 18:07:21 +0530

    Initial Commit

---
+++ countdown-check.js
@@ -0,0 +1,20 @@
+// Countdown timer check - redirects to countdown page if timer hasn't finished
+(function() {
+  // Set the same target date/time as in countdown.html
+  const targetDateTime = '2026-02-19 18:30:00';
+  
+  // Don't redirect if we're already on the countdown page
+  if (window.location.pathname.endsWith('countdown.html')) {
+    return;
+  }
+  
+  // Check if countdown has finished
+  const targetDate = new Date(targetDateTime).getTime();
+  const now = new Date().getTime();
+  const distance = targetDate - now;
+  
+  // If countdown hasn't finished, redirect to countdown page
+  if (distance > 0) {
+    window.location.href = 'countdown.html';
+  }
+})();
