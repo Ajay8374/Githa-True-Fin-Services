faad02e16095380f0869a9d1302b6a7e58b2e406
Ajay8374 <132041577+Ajay8374@users.noreply.github.com>
2026-02-20 14:46:23 +0530

    Update countdown-check.js
    TIME

--- countdown-check.js
+++ countdown-check.js
@@ -1,7 +1,7 @@
 // Countdown timer check - redirects to countdown page if timer hasn't finished
 (function() {
   // Set the same target date/time as in countdown.html
-  const targetDateTime = '2026-02-20 14:40:00';
+  const targetDateTime = '2026-02-20 15:40:00';
   
   // Don't redirect if we're already on the countdown page
   if (window.location.pathname.endsWith('countdown.html')) {
